a1909f434f926dbd0d39aeeb0c079614798defba
Aditya Rathi <aditya.rathi@vedantu.com>
2019-04-11 15:22:52 +0530

    Redux Complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import List from './js/components/List';
+import Form from './js/components/Form';
+import Posts from './js/components/Posts';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="row mt-5">
+        <div className="col-md-4 offset-md-1">
+          <h2>Articles</h2>
+          <List/>
+        </div>
+        <div className="col-md-4 offset-md-1">
+          <h2>Add new Article</h2>
+          <Form/>
+        </div>
+        <div className="col-md-4 offset-md-1">
+          <h2>Remote Articles</h2>
+          <Posts/>
+        </div>
+        
       </div>
     );
   }
